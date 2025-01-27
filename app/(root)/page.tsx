ddef79b5e57abe852bc7288f4c210b86cb794617
nirmalks <kagosekar@gmail.com>
2025-01-27 17:00:42 +0530

    1. add shadcn setup and button 2. setup basic metadata 3. add basic layout with header and footer

---
+++ app/(root)/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return <h1>Bookstore</h1>;
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.APP_NAME || 'Bookstore'
+export const APP_DESCRIPTION = process.env.APP_DESCRIPTION || 'Ecommerce platform for books'
+export const SERVER_URL = process.env.SERVER_URL || "http://localhost:3000"
